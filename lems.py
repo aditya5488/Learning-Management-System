ba5e2592b53b4c20b0828c08fcd83dae64c5bc86
aditya5488 <adityachaurasiya5488@gmail.com>
2026-09-10 13:44:36 +0530

    v1.0-Basic Course Details

---
+++ lems.py
@@ -0,0 +1,8 @@
+# Learning Management System - Version 1.0
+# Feature: enroll and complete courses
+
+def enroll_course(course_id, student_id):
+    print("Student", student_id, "enrolled in course", course_id)
+
+def complete_course(course_id, student_id):
+    print("Student", student_id, "completed course", course_id)
