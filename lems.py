b8f6a769e02733e914e4eeba085ce11a7e8fcc27
aditya5488 <adityachaurasiya5488@gmail.com>
2026-09-10 13:53:33 +0530

    v2.0-added online catalogue

--- lems.py
+++ lems.py
@@ -1,5 +1,7 @@
-# Learning Management System - Version 1.1
-# Features: enroll and complete courses, fee calculation
+# Learning Management System - Version 2.0
+# Features: enroll/complete courses, fee calculation, online course search
+
+catalogue = ["Python", "DBMS", "Operating Systems"]
 
 def enroll_course(course_id, student_id):
     print("Student", student_id, "enrolled in course", course_id)
@@ -11,4 +13,10 @@ def calculate_fee(course_fee, discount=0):
     fee = course_fee - discount
     print("Course Fee = Rs.", fee)
     return fee
+
+def search_course(title):
+    if title in catalogue:
+        print(title, "is available")
+    else:
+        print(title, "not found")
 
